e6c1fe6086cf452682e183bce9c469844501923d
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-02 22:27:38 +0700

    Add `svelte-fa`

--- crypta-v2/src/routes/+layout.svelte
+++ crypta-v2/src/routes/+layout.svelte
@@ -1,61 +1,54 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+import "../app.postcss";
+import { AppShell, AppBar } from "@skeletonlabs/skeleton";
 
-	// Highlight JS
-	import hljs from 'highlight.js/lib/core';
-	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-	import xml from 'highlight.js/lib/languages/xml'; // for HTML
-	import css from 'highlight.js/lib/languages/css';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import typescript from 'highlight.js/lib/languages/typescript';
+// Highlight JS
+// import hljs from "highlight.js/lib/core";
+// import "highlight.js/styles/github-dark.css";
+// import { storeHighlightJs } from "@skeletonlabs/skeleton";
+// import xml from "highlight.js/lib/languages/xml"; // for HTML
+// import css from "highlight.js/lib/languages/css";
+// import javascript from "highlight.js/lib/languages/javascript";
+// import typescript from "highlight.js/lib/languages/typescript";
 
-	hljs.registerLanguage('xml', xml); // for HTML
-	hljs.registerLanguage('css', css);
-	hljs.registerLanguage('javascript', javascript);
-	hljs.registerLanguage('typescript', typescript);
-	storeHighlightJs.set(hljs);
+// hljs.registerLanguage("xml", xml); // for HTML
+// hljs.registerLanguage("css", css);
+// hljs.registerLanguage("javascript", javascript);
+// hljs.registerLanguage("typescript", typescript);
+// storeHighlightJs.set(hljs);
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+// Floating UI for Popups
+import {
+	computePosition,
+	autoUpdate,
+	flip,
+	shift,
+	offset,
+	arrow,
+} from "@floating-ui/dom";
+import { storePopup } from "@skeletonlabs/skeleton";
+storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar slotDefault="place-self-center">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Crypta</strong>
 			</svelte:fragment>
+			<div class="input-group input-group-divider mb-2">
+        <div class="input-group-shim">
+          <i class="fa-solid fa-search"></i>
+        </div>
+        <input
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<strong class="text-xl uppercase invisible">Crypta</strong>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
