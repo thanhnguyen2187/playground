5561af4efb408fe0327ea3c0614fad95920fc7a5
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-03 17:42:10 +0700

    Test Triplit

--- crypta-v2/src/routes/playground/+page.svelte
+++ crypta-v2/src/routes/playground/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-  import NoteItemList from '../../components/NoteItemList.svelte';
+import NoteItemList from "../../components/NoteItemList.svelte";
+import { TriplitClient } from "@triplit/client";
+
+const client = new TriplitClient();
+
+let result = "loading...";
+
+(async () => {
+	const insertResult = await client.insert("notes", { title: "Hello world" });
+	await client.insert("notes", { title: "Goodbye world" });
+	const query = client.query("notes").select(["id", "title"]).build();
+	result = await client.fetch(query, { policy: "local-only" });
+})();
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <NoteItemList/>
+  {JSON.stringify(result)}
 </div>
