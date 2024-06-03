ff7f8cabc292a68b8faabb99a86f7dd965c3933f
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-04 00:36:43 +0700

    Reformat `wa-sqlite.test.ts`

--- crypta-v2/src/data/wa-sqlite.test.ts
+++ crypta-v2/src/data/wa-sqlite.test.ts
@@ -1,56 +1,50 @@
-import { describe, it, expect, beforeAll, afterAll } from 'vitest'
-import { setupServer } from 'msw/node'
-import {
-  createDb,
-  createSQLiteAPI,
-  createQueryExecutor,
-} from './wa-sqlite'
-import type { WASQLiteExecutor } from './wa-sqlite'
-import type { SqliteRemoteDatabase } from 'drizzle-orm/sqlite-proxy'
-import { createWASqliteMockWASMHandler } from './testing'
-import { sql } from 'drizzle-orm'
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { setupServer } from "msw/node";
+import { createDb, createSQLiteAPI, createQueryExecutor } from "./wa-sqlite";
+import type { WASQLiteExecutor } from "./wa-sqlite";
+import type { SqliteRemoteDatabase } from "drizzle-orm/sqlite-proxy";
+import { createWASqliteMockWASMHandler } from "./testing";
+import { sql } from "drizzle-orm";
 
-const handlers = createWASqliteMockWASMHandler()
-const server = setupServer(...handlers)
+const handlers = createWASqliteMockWASMHandler();
+const server = setupServer(...handlers);
 beforeAll(() => {
-  server.listen({onUnhandledRequest: 'error'})
-})
+	server.listen({ onUnhandledRequest: "error" });
+});
 afterAll(() => {
-  server.close()
-})
+	server.close();
+});
 
-describe('happy path', async () => {
+describe("happy path", async () => {
+	let sqliteAPI: SQLiteAPI;
+	let executor: WASQLiteExecutor;
+	let localDb: SqliteRemoteDatabase;
 
-  let sqliteAPI: SQLiteAPI
-  let executor: WASQLiteExecutor
-  let localDb: SqliteRemoteDatabase
+	beforeAll(async () => {
+		sqliteAPI = await createSQLiteAPI("http://mock.local", "MemoryAsyncVFS");
+		executor = await createQueryExecutor(sqliteAPI, "crypta", false);
+		localDb = createDb(executor);
+	});
 
-  beforeAll(async () => {
-    sqliteAPI = await createSQLiteAPI('http://mock.local', 'MemoryAsyncVFS')
-    executor = await createQueryExecutor(sqliteAPI, 'crypta', false)
-    localDb = createDb(executor)
-  })
+	it("basic query", async () => {
+		{
+			const result = await executor.execute("SELECT 1");
+			expect(result).toEqual([[1]]);
+		}
+		{
+			const result = await executor.execute("PRAGMA user_version");
+			expect(result).toEqual([[0]]);
+		}
+	});
 
-  it('basic query', async () => {
-    {
-      const result = await executor.execute('SELECT 1')
-      expect(result).toEqual([[1]])
-    }
-    {
-      const result = await executor.execute('PRAGMA user_version')
-      expect(result).toEqual([[0]])
-    }
-  })
-
-  it('local db', async () => {
-    {
-      const result = await localDb.run(sql`SELECT 1`)
-      expect(result).toEqual({rows: [[1]]})
-    }
-    {
-      const result = await localDb.run(sql`PRAGMA user_version`)
-      expect(result).toEqual({rows: [[0]]})
-    }
-  })
-
-})
+	it("local db", async () => {
+		{
+			const result = await localDb.run(sql`SELECT 1`);
+			expect(result).toEqual({ rows: [[1]] });
+		}
+		{
+			const result = await localDb.run(sql`PRAGMA user_version`);
+			expect(result).toEqual({ rows: [[0]] });
+		}
+	});
+});
