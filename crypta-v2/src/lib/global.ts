f4f18da4cb5131fc0bf03f7fc9a7b1297bb726c1
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-06-08 01:10:44 +0700

    Make IndexedDB persistence works

--- crypta-v2/src/lib/global.ts
+++ crypta-v2/src/lib/global.ts
@@ -1,4 +1,4 @@
 import { TriplitClient } from '@triplit/client';
 import { schema } from '../data/schema-triplit';
 
-export const globalClient = new TriplitClient({ schema });
+export const globalClient = new TriplitClient({ schema, storage: "indexeddb" });
