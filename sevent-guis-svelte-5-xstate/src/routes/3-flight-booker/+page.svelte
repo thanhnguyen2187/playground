f2cfcfcf7f1deef6ab985913554f9a3d5592ed29
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-11-04 16:29:31 +0700

    Draft Flight Booker UI

---
+++ sevent-guis-svelte-5-xstate/src/routes/3-flight-booker/+page.svelte
@@ -0,0 +1,24 @@
+<div class="flex flex-col">
+  <div class="form-control">
+    <div class="label">
+      <span class="label-text">Flight type</span>
+    </div>
+    <select class="select select-bordered w-60">
+      <option value="1">one-way flight</option>
+      <option value="2">return flight</option>
+    </select>
+  </div>
+  <div class="form-control">
+    <div class="label">
+      <span class="label-text">Start date</span>
+    </div>
+    <input class="input input-bordered w-60">
+  </div>
+  <div class="form-control">
+    <div class="label">
+      <span class="label-text">Return date</span>
+    </div>
+    <input class="input input-bordered w-60">
+  </div>
+  <button class="btn btn-primary mt-4 w-32">Book flight</button>
+</div>
