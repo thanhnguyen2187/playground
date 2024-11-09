dec82c79214d47bd16ec4b421428244ca987c958
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-11-09 18:21:08 +0700

    Draft echo server with websocket

--- chat-svelte-xstate-websocket/packages/server/index.ts
+++ chat-svelte-xstate-websocket/packages/server/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+import uWS from "uWebSockets.js";
+
+uWS
+  .App()
+  .ws("/*", {
+    open: (ws) => {
+      ws.send("Hello world!");
+    },
+    message: (ws, message, isBinary) => {
+      ws.send(message);
+    },
+    close: () => {},
+  })
+  .listen(8080, () => {});
