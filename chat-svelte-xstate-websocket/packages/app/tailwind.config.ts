dc4c9b32604bc261b5c709faa71bb037fc581b1a
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-11-09 17:32:50 +0700

    Add DaisyUI and XState

--- chat-svelte-xstate-websocket/packages/app/tailwind.config.ts
+++ chat-svelte-xstate-websocket/packages/app/tailwind.config.ts
@@ -1,15 +1,16 @@
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import containerQueries from '@tailwindcss/container-queries';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import containerQueries from "@tailwindcss/container-queries";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {}
+    extend: {},
   },
 
-  plugins: [typography, forms, containerQueries, aspectRatio]
+  plugins: [typography, forms, containerQueries, aspectRatio, daisyui],
 } satisfies Config;
