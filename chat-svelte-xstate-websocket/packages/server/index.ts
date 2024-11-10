bb3a7193e58029ae29a09b73e5c585f1db5bba66
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-11-10 14:22:23 +0700

    Make chat room works

--- chat-svelte-xstate-websocket/packages/server/index.ts
+++ chat-svelte-xstate-websocket/packages/server/index.ts
@@ -1,14 +1,37 @@
 import uWS from "uWebSockets.js";
 
+// Set to store all connected WebSocket clients
+const clients = new Set<uWS.WebSocket<unknown>>();
+
 uWS
   .App()
   .ws("/*", {
+    // When a new client connects
     open: (ws) => {
-      ws.send("Hello world!");
+      // Add the client to our set
+      clients.add(ws);
+      ws.send("Connected to chat server!");
     },
+
+    // When a message is received
     message: (ws, message, isBinary) => {
-      ws.send(message);
+      // Broadcast to all clients except sender
+      for (const client of clients) {
+        // Make sure client isn't the sender and is still connected
+        if (client !== ws && client.getBufferedAmount() < 16384) {
+          client.send(message, isBinary);
+        }
+      }
     },
-    close: () => {},
+
+    // When a client disconnects
+    close: (ws) => {
+      // Remove the client from our set
+      clients.delete(ws);
+    },
   })
-  .listen(8080, () => {});
+  .listen(8080, (listenSocket) => {
+    if (listenSocket) {
+      console.log("Listening on port 8080");
+    }
+  });
