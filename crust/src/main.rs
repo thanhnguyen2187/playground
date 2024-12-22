af4d0bcb1d3afe541d788288bfd99f985d2261a5
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-12-22 21:47:16 +0700

    Implement `crust`, a `cat` clone in Rust

---
+++ crust/src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs;
+
+// enum State {
+//     Help,
+//     Version,
+//     Run(String),
+// }
+
+fn print_help() {
+    println!("crust, a cat clone written in Rust");
+    println!();
+    println!("Usage:");
+    println!();
+    println!("  crust [file-path]   print the file's contents");
+    println!("  crust --help        print this help message");
+    println!("  crust --version     print the version number");
+}
+
+fn print_version() {
+    println!("v0.1.0");
+}
+
+fn run(file_path: &str) {
+    let contents = fs::read_to_string(file_path).expect("file not found");
+    println!("{}", contents);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => match args[0].as_str() {
+            "--help" => print_help(),
+            "--version" => print_version(),
+            _ => run(&args[1]),
+        },
+        _ => print_help(),
+    }
+}
