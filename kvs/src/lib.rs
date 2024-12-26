800b9d282be68f00f0f32acd966b65fd6ee349f1
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-12-27 01:47:27 +0700

    kvs: make KvStore works

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,19 +1,25 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    underlying: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            underlying: HashMap::new(),
+        }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-        unimplemented!()
+        self.underlying.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+        self.underlying.get(&key).cloned()
     }
 
     pub fn remove(&mut self, key: String) {
-        unimplemented!()
+        self.underlying.remove(&key);
     }
 }
