3c095e54d0c621d4fe8f624d01f5ea2e61689173
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-01-09 16:02:08 +0700

    kvs: make TCP server CLI works

--- kvs/src/bin/server_tcp.rs
+++ kvs/src/bin/server_tcp.rs
@@ -1,5 +1,20 @@
+use env_logger::Env;
+use kvs::Result;
+use log::{error, info};
+use std::env;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
+use clap::Parser;
+
+use cli::engine::{check_engine_db_file, Engine};
+use cli::server::Server;
+use cli::parse_addr::parse_addr;
+
+mod cli {
+    pub mod engine;
+    pub mod server;
+    pub mod parse_addr;
+}
 
 fn handle_connection(stream: TcpStream) -> Vec<String> {
     let buf_reader = BufReader::new(&stream);
@@ -19,14 +34,40 @@ fn response_connection(stream: TcpStream, response: Vec<String>) {
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4004").unwrap();
+fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+    info!("Logger initialized!");
+    info!("Current binary version: {:?}", env!("CARGO_PKG_VERSION"));
 
-    println!("Listening on http://127.0.0.1:4004");
+    let cli = Server::parse();
+    // TODO: validate engine by Clap instead of hard-coding
+    parse_addr(&cli.addr)?;
+    info!("Started server at: {:?}", cli.addr);
+    info!("Chosen engine: {:?}", {
+        match cli.engine {
+            Engine::Kvs => "kvs",
+            Engine::Sled => "sled",
+            Engine::Mem => "in-memory",
+        }
+    });
+
+    if let Err(err) = check_engine_db_file(&cli.engine) {
+        error!(
+            "Database file of engines other than {} already exists",
+            cli.engine,
+        );
+        return Err(err);
+    }
+
+    // let current_dir = env::current_dir().unwrap();
+    let listener = TcpListener::bind(cli.addr).unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let lines = handle_connection(stream.try_clone().unwrap());
         println!("Received request: {:?}", lines);
         response_connection(stream, lines);
     }
+
+    Ok(())
 }
