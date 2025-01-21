7abfc3d5bce4d77d702e8e3d842369669ac49df0
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-01-21 14:42:24 +0700

    7guis-harm: simplify Counter code

--- seven-guis-htmx-axum-rust-maud/src/counter.rs
+++ seven-guis-htmx-axum-rust-maud/src/counter.rs
@@ -7,8 +7,8 @@ use crate::common::{header, home_back_link};
 pub async fn page(
     State(app_state_arc): State<Arc<Mutex<AppState>>>,
 ) -> Markup {
-    let server_component = if let Ok(app_state) = app_state_arc.lock() {
-        counter_component(&app_state)
+    let input = if let Ok(app_state) = app_state_arc.lock() {
+        counter_input(&app_state)
     } else {
         html! {
             "Unable to get app state"
@@ -20,7 +20,21 @@ pub async fn page(
         body {
             h1 { "Counter" }
 
-            (server_component)
+        form {
+            fieldset {
+                label {
+                    "Server value: "
+                    (input)
+                }
+                button
+                    type="submit"
+                    hx-post="/counter-increase"
+                    hx-target="#counter-input"
+                    hx-swap="outerHTML"
+                    hx-trigger="click"
+                    { "Increment" };
+            }
+        }
 
             form x-data="{ count: 0 }" {
                 fieldset {
@@ -39,23 +53,14 @@ pub async fn page(
     }
 }
 
-pub fn counter_component(app_state: &AppState) -> Markup {
+pub fn counter_input(app_state: &AppState) -> Markup {
     html! {
-        form #counter {
-            fieldset {
-                label {
-                    "Server value: "
-                    input type="number" value=(app_state.counter) name="counter";
-                }
-                button
-                    type="submit"
-                    hx-post="/counter-increase"
-                    hx-target="#counter"
-                    hx-swap="outerHTML"
-                    hx-trigger="click"
-                    { "Increment" };
-            }
-        }
+        input
+            #counter-input
+            type="number"
+            value=(app_state.counter)
+            name="counter"
+        ;
     }
 }
 
@@ -64,7 +69,7 @@ pub async fn increase(
 ) -> Markup {
     let data = if let Ok(mut app_state) = app_state_arc.lock() {
         app_state.counter += 1;
-        counter_component(&app_state)
+        counter_input(&app_state)
     } else {
         html! {
             "Unable to get app state"
