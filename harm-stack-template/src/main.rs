1e30091bc2f50b33f4bec4180ab785cc8b103a9b
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-02-05 22:07:23 +0700

    harm-stack-template: add `tower_livereload`

--- harm-stack-template/src/main.rs
+++ harm-stack-template/src/main.rs
@@ -3,12 +3,14 @@ mod templates;
 use crate::templates::page_unimplemented;
 use axum::{routing::get, Router};
 use tower_http::services::ServeFile;
+use tower_livereload::LiveReloadLayer;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(page_unimplemented))
-        .route_service("/styles.css", ServeFile::new("./static/styles.css"));
+        .route_service("/styles.css", ServeFile::new("./static/styles.css"))
+        .layer(LiveReloadLayer::new());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
